95bfb2b321b5429048309777afd84fbc9228aba8
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-02-09 20:12:30 -0500

    // I have created the variable with confirm method for the Numbers & special Characters if they selected yes they want numbers & special characters and then they will get a prompt that your password will have special characters and numbers  else not & I also added a if condition if user say no to lowercase,uppercase,numbers and special characters then they will get a msg to select atleats onecharacter type

--- script.js
+++ script.js
@@ -25,21 +25,46 @@ function generatePassword(){
   // I have created the variable with confirm method for the lower case if they selected lowercase and then they will get a prompt that your password will have lowecase else not
   var hasLowercase = confirm("Do you want lowercase characters?");
   if (hasLowercase) {
-    var turnToLowercase = alert("Your password will have lowercase characters.");
+    alert("Your password will have lowercase characters.");
   }
   else {
     alert("Your password will NOT have lowercase characters.");
   }
 
-  // I have created the variable with confirm method for the lower case if they selected uppercase and then they will get a prompt that your password will have uppercase else not
+  // I have created the variable with confirm method for the upper case if they selected uppercase and then they will get a prompt that your password will have uppercase else not
 
   var hasUppercase = confirm("Do you want uppercase characters?");
 if (hasUppercase) {
-  var turnToUppercase = alert("Your password will have lowercase characters.");
+  alert("Your password will have lowercase characters.");
 }
 else {
   alert("Your password will NOT have lowercase characters.");
 }
+
+// I have created the variable with confirm method for the number if they selected yes they want number and then they will get a prompt that your password will have numbers else not
+
+var hasNumbers = confirm("Do you want to use numbers?");
+if (hasNumbers) {
+  alert("Your password will have numbers.");
+}
+else {
+  alert("Your password will NOT have numbers.");
+}
+
+// I have created the variable with confirm method for the special Characters if they selected yes they want special characters and then they will get a prompt that your password will have special characters else not
+
+var hasSpecial = confirm("Do you want special characters?");
+  if (hasSpecial) {
+    alert("Your password will have special characters.");
+  }
+  else {
+    alert("Your password will NOT have special characters.");
+  }
+
+  // I created a If condition that states that if the user says no to lowercase,uppercase,numbers & special characted to be in his/her password then it willr eturn please select atleast one type 
+  if (hasLowercase === false && hasUppercase === false && hasNumbers === false && hasSpecial === false) {
+    return "Please select at least one character type.";
+  };
 
 }
 
